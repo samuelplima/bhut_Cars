1ed5736aa944282e82633f54b4d7980e5378293c
Samuel Lima <samuel.lima@voffice.com.br>
2022-04-12 14:28:38 -0300

    ajuste no envio de webhook

--- src/main/java/br/com/bhut/cars/rabbit/QueueConsumer.java
+++ src/main/java/br/com/bhut/cars/rabbit/QueueConsumer.java
@@ -1,30 +1,31 @@
 package br.com.bhut.cars.rabbit;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import br.com.bhut.cars.client.ClientWebhookPost;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 
 @Component
 public class QueueConsumer implements Serializable {
 
     private String myString;
+    private ClientWebhookPost clientWebhookPost;
+
+    @Autowired
+    public QueueConsumer(ClientWebhookPost clientWebhookPost) {
+        this.clientWebhookPost = clientWebhookPost;
+    }
 
     @RabbitListener(queues = {"${queue.name}"})
     public void receive(@Payload Message message) {
         byte[] byteArray = (byte[]) message.getPayload();
         myString = ("New car created: " + new String(byteArray));
+        clientWebhookPost.PostWebHook(myString);
     }
 
 }
